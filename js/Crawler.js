0ed626b6c8d91f1eb66437778549bc34ba10184c
zstring <jai.himanshu001@gmail.com>
2014-11-26 19:52:10 -0500

    Updated and displayed data from solr based on user query

--- js/Crawler.js
+++ js/Crawler.js
@@ -6,24 +6,48 @@ function calc(numA, numB) {
 }
 
 function callSolr() {
-	http://98.118.151.224:8983/solr/newscollection/select?q=abc&sort=NEWSDATE+desc&wt=json&indent=true
-	var solrApi = "http://98.118.151.224:8983/solr/collection1/select?q=";
-	solrApi = solrApi + $("input").val() + "&wt=json&indent=true";
+	//98.118.151.224
+	var solrApi = "http://98.118.151.224:8983/solr/newscollection/select";//?q=";
 	sentences[0] = "";
-	$.getJSON(solrApi).
-		done(function(data) {
-			$("#result").empty();
-			$.each(data.response.docs, function(i, doc){
-				var html = '<li> Date: ' + this.NEWSDATE + '</br>';
-				html += ' Place: '+ this.PLACE + '</br>';;
-				html += ' <b>TITLE: ' + this.TITLE + '</b>'+ '</br>';
-				html += ' Content: ' + this.Content + '</br>';
-				html += ' <a href=\"' + this.Category + '\">Category</a></li></br></br>';
-				$("#results").append(html);
-				sentences[0] = sentences[0].concat(this.lead_paragraph, " ");
-				sentences[0] = sentences[0].concat(this.headline.main, " ");
-			});
+	$("#results").empty();
+	$.ajax({
+		  'url': solrApi,
+		  'data': {'wt':'json', 'q': $("input").val()},
+		  'success': function(data) { 
+					$("#result").empty();
+					$.each(data.response.docs, function(i, doc){
+						var html = '<li> Date: ' + this.NEWSDATE + '</br>';
+						html += ' Place: '+ this.PLACE + '</br>';;
+						html += ' <b>TITLE: ' + this.TITLE + '</b>'+ '</br>';
+						html += ' Content: ' + this.CONTENT + '</br>';
+						html += ' <a href=\"' + this.CATEGORY + '\">Category</a></li></br></br>';
+						$("#results").append(html);
+						sentences[0] = sentences[0].concat(this.TITLE, " ");
+						sentences[0] = sentences[0].concat(this.CONTENT, " ");
+					});
+					topicise();
+		  },
+		  'dataType': 'jsonp',
+		  'jsonp': 'json.wrf'
 		});
+//	$.getJSON(solrApi).
+//		done(function(data) {
+//			$("#result").empty();
+//			$.each(data.response.docs, function(i, doc){
+//				var html = '<li> Date: ' + this.NEWSDATE + '</br>';
+//				html += ' Place: '+ this.PLACE + '</br>';;
+//				html += ' <b>TITLE: ' + this.TITLE + '</b>'+ '</br>';
+//				html += ' Content: ' + this.CONTENT + '</br>';
+//				html += ' <a href=\"' + this.CATEGORY + '\">Category</a></li></br></br>';
+//				$("#results").append(html);
+//			});
+//		})
+//	  .fail(function(data, d) {
+//		    console.log( "error" );
+//		  })
+//	  .always(function(data) {
+//		    console.log( "complete" );
+//		 });
 }
 
 function crawlNYTimes() {
