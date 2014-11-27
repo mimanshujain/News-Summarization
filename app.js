c3f2dd822ec61c1ecd6336d3b8896e607fa98017
Mimanshu Shisodia <mimanshu@buffalo.edu>
2014-11-27 14:41:42 -0800

    nodejs Updates

--- app.js
+++ app.js
@@ -1,12 +1,11 @@
 var express = require('express');
 var path = require('path');
-var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var http = require('http');
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
+var routes = require('./routes');
 
 var app = express();
 
@@ -14,16 +13,20 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(__dirname + '/public/favicon.ico'));
-app.use(logger('dev'));
+
+
+//Adding these express middleware 'layers' to the middleware stack. Each layer is essentially adding
+//a function that specifically handles something to flow through the middleware.
+app.use(logger('dev')); // log incoming requests to the console
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+//serve static content
 app.use(express.static(path.join(__dirname, 'public')));
 
+//var data = require('./data.json');
+//app.locals.appdata = data;
 app.use('/', routes);
-app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,76 @@
 var express = require('express');
 var router = express.Router();
 
+var solr = require('solr-client');
+var querystring = require('querystring');
+var JSONStream = require('JSONStream');
+var fs = require('fs');
+
+// Create a client 
+//Signature :: function createClient(host, port, core, path, agent, secure, bigint)
+var client = solr.createClient('98.118.151.224', 8983, 'newscollection', '/solr', false,false);
+var queryVal = '';
+var result = {};
 /* GET home page. */
-router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res) {
+    if (req.method == 'GET')
+        console.log('GET');
+    res.render('index', { title: 'Mimanshu Shisodia', query: 'Random Query' });
 });
 
+router.post('/', function (req, res) {
+    console.log(req.method);   
+    if (typeof req.body.query != 'undefined') {
+        //console.log('Query: ' + req.body.query.toString());
+        queryVal = req.body.query.toString();
+        var resultSet;
+        result = processQuery(resultSet, callback(queryVal))
+        {
+            return resultSet;
+        };
+    }
+    console.log(result);
+    if (typeof req.body.query != 'undefined')
+        res.render('index', { title: 'Shisodia Mimanshu', query: 'Random Query', text: 'Success ' + req.body.query });
+});
+
+var callback = function (queryVal) {
+    var query = client.createQuery()
+				   .q(queryVal)
+				   .start(0)
+				   .rows(10);
+    client.search(query, function (err, obj) {
+        if (err) {
+            console.log(err);
+        } else {
+            console.log('Mimanshu');
+            console.log(obj);
+            resultSet = obj;
+            fs.writeFile('./message.json', JSON.stringify(resultSet), function (err) {
+                if (err)
+                    console.log(err);
+                else
+                    console.log('message.json saved!');
+            });
+        }
+    });
+    return resultSet;
+}
+
+//function processQuery(result) {
+    
+//    // DixMax query
+//    //var query = client.createQuery()
+//				//  .q(queryVal)
+//				//  .dismax()
+//				//  .mm(2)
+//				//  .start(0)
+//				//  .rows(10)
+//    fs.writeFile('./message.txt', JSON.stringify(result), function (err) {
+//        if (err)
+//            console.log(err);
+//        else
+//            console.log('It\'s saved!');
+//    });
+//}
 module.exports = router;
