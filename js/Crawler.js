a6aa82d35624adc1cf19dfe5cdd6bccc2129ad03
aayushuppal <aayushup@buffalo.edu>
2014-11-27 16:56:16 -0500

    support for guardian api.

--- js/Crawler.js
+++ js/Crawler.js
@@ -4,16 +4,15 @@
 function calc(numA, numB) {
 	return numA + numB;
 }
-var query = [];
+
 function callSolr() {
 	//98.118.151.224
 	var solrApi = "http://98.118.151.224:8983/solr/newscollection/select";//?q=";
 	sentences[0] = "";
 	$("#results").empty();
-	var userQuer = $("input").val();
 	$.ajax({
 		  'url': solrApi,
-		  'data': {'wt':'json', 'q': userQuer},
+		  'data': {'wt':'json', 'q': $("input").val()},
 		  'success': function(data) { 
 					$("#result").empty();
 					$.each(data.response.docs, function(i, doc){
@@ -26,16 +25,7 @@ function callSolr() {
 						sentences[0] = sentences[0].concat(this.TITLE, " ");
 						sentences[0] = sentences[0].concat(this.CONTENT, " ");
 					});
-					topicText = topicise();
-					query = "( " + userQuer + " ) OR "
-					for (var i=0; i<topicText.length && i < 5; i++) {
-						query = query.concat("( " + userQuer + " AND " + topicText[i] + " )");
-						if (i != topicText.length - 1) {
-							query = query.concat(" OR ");
-						}
-					}
-//					crawlNYTimes();
-					crawlGuardian()
+					topicise();
 		  },
 		  'dataType': 'jsonp',
 		  'jsonp': 'json.wrf'
@@ -62,7 +52,7 @@ function callSolr() {
 
 function crawlNYTimes() {
 	var NYTApi = "http://api.nytimes.com/svc/search/v2/articlesearch.json?q=";
-	NYTApi = NYTApi + query + "&sort=newest&api-key=0eadf8a8e685079f3f53202a194920f6:10:70223982";
+	NYTApi = NYTApi + $("input").val() + "&sort=newest&api-key=0eadf8a8e685079f3f53202a194920f6:10:70223982";
 	sentences[0] = "";
 	$.getJSON(NYTApi).
 		done(function(data) {
@@ -82,23 +72,22 @@ function crawlNYTimes() {
 }
 
 function crawlGuardian() {
-	
-		var Api = "http://content.guardianapis.com/search?q=";
-		Api = Api + query + "&api-key=neukrcw8u9xm4ks5zejvx3uj";
-		sentences[0] = "";
-		$.getJSON(Api).
-			done(function(data) {
+	var GApi = "http://content.guardianapis.com/search?q=";
+	GApi = GApi + $("input").val() + "&sort=newest&api-key=neukrcw8u9xm4ks5zejvx3uj";
+	sentences[0] = "";
+	$.getJSON( GApi, function( data ) {
 			$("#results").empty();
-			$.each(data.response.results, function(i, doc){
+			$.each(data.response.results, function(){
 				var html = '<li> Date: ' + this.webPublicationDate + '</br>';
-				html += ' sectionId: '+ this.sectionId + '</br>';;
+				html += ' abstract: '+ this.sectionName + '</br>';;
 				html += ' <b>headlines: ' + this.webTitle + '</b>'+ '</br>';
-//				html += ' leadparagraph: ' + this.lead_paragraph + '</br>';
+				html += ' leadparagraph: ' + this.sectionName + '</br>';
 				html += ' <a href=\"' + this.webUrl + '\">Click here</a></li></br></br>';
 				$("#results").append(html);
-				sentences[0] = sentences[0].concat(this.sectionId, " ");
 				sentences[0] = sentences[0].concat(this.webTitle, " ");
+				sentences[0] = sentences[0].concat(this.sectionName, " ");
+				
 			});
 			topicise();
-	});
+		});
 }
