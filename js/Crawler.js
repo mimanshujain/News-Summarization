0ddb476fb8ffb5ab13bb69ea934dd3f7e29f9c63
zstring <jai.himanshu001@gmail.com>
2014-11-27 16:45:46 -0500

    Updated Guardian API call

--- js/Crawler.js
+++ js/Crawler.js
@@ -4,15 +4,16 @@
 function calc(numA, numB) {
 	return numA + numB;
 }
-
+var query = [];
 function callSolr() {
 	//98.118.151.224
 	var solrApi = "http://98.118.151.224:8983/solr/newscollection/select";//?q=";
 	sentences[0] = "";
 	$("#results").empty();
+	var userQuer = $("input").val();
 	$.ajax({
 		  'url': solrApi,
-		  'data': {'wt':'json', 'q': $("input").val()},
+		  'data': {'wt':'json', 'q': userQuer},
 		  'success': function(data) { 
 					$("#result").empty();
 					$.each(data.response.docs, function(i, doc){
@@ -25,7 +26,16 @@ function callSolr() {
 						sentences[0] = sentences[0].concat(this.TITLE, " ");
 						sentences[0] = sentences[0].concat(this.CONTENT, " ");
 					});
-					topicise();
+					topicText = topicise();
+					query = "( " + userQuer + " ) OR "
+					for (var i=0; i<topicText.length && i < 5; i++) {
+						query = query.concat("( " + userQuer + " AND " + topicText[i] + " )");
+						if (i != topicText.length - 1) {
+							query = query.concat(" OR ");
+						}
+					}
+//					crawlNYTimes();
+					crawlGuardian()
 		  },
 		  'dataType': 'jsonp',
 		  'jsonp': 'json.wrf'
@@ -52,7 +62,7 @@ function callSolr() {
 
 function crawlNYTimes() {
 	var NYTApi = "http://api.nytimes.com/svc/search/v2/articlesearch.json?q=";
-	NYTApi = NYTApi + $("input").val() + "&sort=newest&api-key=0eadf8a8e685079f3f53202a194920f6:10:70223982";
+	NYTApi = NYTApi + query + "&sort=newest&api-key=0eadf8a8e685079f3f53202a194920f6:10:70223982";
 	sentences[0] = "";
 	$.getJSON(NYTApi).
 		done(function(data) {
@@ -69,4 +79,26 @@ function crawlNYTimes() {
 			});
 			topicise();
 		});
+}
+
+function crawlGuardian() {
+	
+		var Api = "http://content.guardianapis.com/search?q=";
+		Api = Api + query + "&api-key=neukrcw8u9xm4ks5zejvx3uj";
+		sentences[0] = "";
+		$.getJSON(Api).
+			done(function(data) {
+			$("#results").empty();
+			$.each(data.response.results, function(i, doc){
+				var html = '<li> Date: ' + this.webPublicationDate + '</br>';
+				html += ' sectionId: '+ this.sectionId + '</br>';;
+				html += ' <b>headlines: ' + this.webTitle + '</b>'+ '</br>';
+//				html += ' leadparagraph: ' + this.lead_paragraph + '</br>';
+				html += ' <a href=\"' + this.webUrl + '\">Click here</a></li></br></br>';
+				$("#results").append(html);
+				sentences[0] = sentences[0].concat(this.sectionId, " ");
+				sentences[0] = sentences[0].concat(this.webTitle, " ");
+			});
+			topicise();
+	});
 }
