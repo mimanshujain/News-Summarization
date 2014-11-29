afd661d4f7a685fe549d344d65e1e3629b8d799e
Mimanshu Shisodia <mimanshu@buffalo.edu>
2014-11-29 15:06:22 -0800

    TimeLine Integration with Guardian Data Completed

--- routes/index.js
+++ routes/index.js
@@ -19,7 +19,10 @@ var querystring = require('querystring');
 var client = solr.createClient('98.118.151.224', 8983, 'newscollection', '/solr', false, false);
 var queryVal = '';
 var result = {};
-var output = "{\x22timeline\x22:{\x22headline\x22:\x22";
+
+//Initial Set up for Timeline Json
+var output = "{\x22timeline\x22:{\x22headline\x22:\x22\x22,\x22type\x22:\x22default\x22,\x22text\x22:\x22<p></p>\x22,\x22startDate\x22:\x22\"";
+
 
 /* GET home page. */
 router.get('/', function (req, res) {
@@ -46,97 +49,153 @@ router.post('/', function (req, res) {
             if (err) {
                 console.log(err);
             } else {
-                console.log('Saving Result Set');
-                //console.log(obj);
+                console.log('Processing Result Set');
                 result.solrData = obj;
-                result.gaurdianData = crawlGuardian(queryVal);
+                //result.gaurdianData = 
+                //crawlGuardian(queryVal);
+                
+                console.log('inside1');
+                var endpoint = "http://content.guardianapis.com/search";
+                method = 'GET';
+                console.log('inside2');
+                if (method == 'GET') {
+                    endpoint += '?q=' + query + "&api-key=" + key + "&sort=newest";
+                }
+                
+                requestify.get(endpoint).then(function (resp) {
+                    console.log('inside3');
+                    //console(resp.getBody().response.results);
+                    result.gaurdianData = resp.getBody().response.results;
+                    
+                    var solrLength = Object.keys(result.solrData.response.docs).length;
+                    var guarLength = Object.keys(result.gaurdianData).length;
+                    console.log('solrLength::' + solrLength); console.log('guarLength::' + guarLength);
+                    if (solrLength > 0 && guarLength > 0) {
+                        output += ",\x22date\x22:[";
+                        start = 1; console.log('first');
+                        createTimelineJSON(result.solrData.response.docs, false, 'solr');
+                        createTimelineJSON(result.gaurdianData, true, 'guardian');
+                    }
+                    else if (solrLength > 0 && guarLength == 0) {
+                        output += ",\x22date\x22:[";
+                        console.log('second');
+                        createTimelineJSON(result.solrData, true, 'solr');
+                    }
+                    else if (solrLength == 0 && guarLength > 0) {
+                        output += ",\x22date\x22:[";
+                        console.log('third');
+                        createTimelineJSON(result.gaurdianData, true, 'guardian');
+                    }
+                    else {                        console.log('fourth');
+                        output += "}}";
+                    }
+                    
+                    fs.writeFile('./timeLine.json', output, function (err) {
+                        if (err)
+                            console.log(err);
+                        else {
+                            console.log('timeLine.json saved!');
+                        }
+                    });
+                    
+                    result.timeLineData = JSON.parse(output);
+                    
+                    if (typeof req.body.query != 'undefined')
+                        res.send(result);
 
-                var start = 0;
-                if (Object.keys(result.solrData.response.docs).length > 1) {
-                    start = 1;
-                    createTimelineJSON(result.solrData, false, start);
-                }
-                if (Object.keys(result.gaurdianData).length > 1) {
-                    createTimelineJSON(result.gaurdianData, true, start);
-                }
-                
-                
-                
-                result.timeLineData = JSON.parse(output);
-                                
-                if (typeof req.body.query != 'undefined')
-                    res.send(result);
-                //
-                //console.log("Guardian : " +result.data3);
-                //fs.writeFile('./message.json', JSON.stringify(result), function (err) {
-                //    if (err)
-                //        console.log(err);
-                //    else {
-                //        result.data2 = createTimelineJSON(obj);
-                //        console.log('message.json saved!');
-                //        if (typeof req.body.query != 'undefined')                            
-                //            res.send(result);
-                //    }            
-                //});
+        //return response.getBody().response.results;
+
+                });
             }
         });
     }
 });
 
 //o create TimeLine Json Object
-function createTimelineJSON(result, isFinish, type, start) {
-    
-    var data = {};
+function createTimelineJSON(data, isFinish, type) {
+    console.log(type);
+    //var data = {};
     var title = '';
     var content = '';
     var newsDate = '';
     if (Object.keys(data).length == 0)
         return;
-
-    if (type == 'solr' ) {
-        data = result.response.docs;
-        title = replaceAllDouble(data[0].TITLE, "title");
-        content = replaceAllDouble(data[0].CONTENT, "content");
-        output += title + "\x22,\x22type\x22:\x22default\x22,\x22text\x22:\x22<p>";
-        output += content + "</p>\x22,\x22startDate\x22:\x22" + getDate() + "\x22,";
-    }
-    if (type == 'guardian' && start == 0) {
-        data = result;
-        title = replaceAllDouble(data[0].webTitle, "title");
-        content = replaceAllDouble(data[0].id, "content");
-        newsDate = data[0].webPublicationDate.substring(0, 10).split("-");
-        output += title + "\x22,\x22type\x22:\x22default\x22,\x22text\x22:\x22<p>";
-        output += content + "</p>\x22,\x22startDate\x22:\x22" + newsDate + "\x22,";
-        output += "\"tag\":\"" + data[0].sectionName + "\"";
-        start = 1;
-    }
-
-    output += "\x22date\x22:[";
     
-    for (var i = start; i < Object.keys(data).length; i++) {
+    for (var i = 0; i < Object.keys(data).length; i++) {
         
-        //output += "{ \x22startDate\x22:\x22" + getDate() + " \x22,\x22endDate\x22:\x22" + getDate() + "\x22,\x22headline\x22:\x22";
-        output += "{ \x22startDate\x22:\x22" + getDate() + " \x22,\x22headline\x22:\x22";
-        output += replaceAllDouble(data[i].TITLE, "title") + "\x22,\x22text\x22:\x22<p>" + replaceAllDouble(data[i].CONTENT, "content") + "</p>\x22,";
+        if (type == 'solr') {
+            output += "{ \x22startDate\x22:\x22" + getDate() + " \x22,\x22headline\x22:\x22";
+            output += replaceAllDouble(data[i].TITLE, "title") + "\x22,\x22text\x22:\x22<p>" + replaceAllDouble(data[i].CONTENT, "content") + "</p>\x22,";
+        }
+        
+        if (type == 'guardian') {
+            newsDate = data[i].webPublicationDate.substring(0, 10).split("-");
+            newsDate = newsDate[0] + "," + newsDate[1] + "," + newsDate[2];
+            output += "{ \x22startDate\x22:\x22" + newsDate + " \x22,\x22headline\x22:\x22";
+            output += replaceAllDouble(data[i].webTitle, "title") + "\x22,\x22text\x22:\x22<p>" + replaceAllDouble(data[i].id, "content") + "</p>\x22,";
+            output += "\"tag\":\"" + data[i].sectionName + "\",";
+        }
         
         if (i == Object.keys(data).length - 1 && isFinish) {
             output += "\x22asset\x22:{\x22media\x22:\x22\x22,\x22credit\x22:\x22\x22,\x22caption\x22:\x22\x22}}]}}";
         }
         else
             output += "\x22asset\x22:{\x22media\x22:\x22\x22,\x22credit\x22:\x22\x22,\x22caption\x22:\x22\x22}},";
-
-    //},";
     }
+}
+
+function crawlGuardian(query) {
+    //console.log('inside1');
+    //var endpoint = "http://content.guardianapis.com/search";
+    //method = 'GET';
+    //console.log('inside2');    
+    //if (method == 'GET') {
+    //    endpoint += '?q=' + query + "&api-key=" + key + "&sort=newest";
+    //}
     
-    //fs.writeFile('./timeLine.json', output, function (err) {
-    //    if (err)
-    //        console.log(err);
-    //    else {
-    //        console.log('timeLine.json saved!');
+    //requestify.get(endpoint).then(function (response) {
+    //    console.log('inside3');
+    //    console(response.getBody().response.results[0]);
+    //    result.gaurdianData = response.getBody().response.results;
+        
+    //    var solrLength = Object.keys(result.solrData.response.docs).length;
+    //    var guarLength = Object.keys(result.gaurdianData).length;
+        
+    //    if (solrLength > 0 && guarLength > 0) {
+    //        start = 1; console.log('first');
+    //        createTimelineJSON(result.solrData, false, 'solr');
+    //        createTimelineJSON(result.gaurdianData, true, 'guardian');
     //    }
+    //    else if (solrLength > 0 && guarLength == 0) {            console.log('second');
+    //        createTimelineJSON(result.solrData, true, 'solr');
+    //    }
+    //    else if (solrLength == 0 && guarLength > 0) { console.log('third');
+    //        createTimelineJSON(result.gaurdianData, true, 'guardian');
+    //    }
+    //    else {
+    //        output += "}}";
+    //    }
+        
+    //    fs.writeFile('./timeLine.json', output, function (err) {
+    //        if (err)
+    //            console.log(err);
+    //        else {
+    //            console.log('timeLine.json saved!');
+    //        }
+    //    });
+        
+    //    result.timeLineData = JSON.parse(output);
+        
+    //    if (typeof req.body.query != 'undefined')
+    //        res.send(result);
+
+    //    //return response.getBody().response.results;
+
     //});
-    
-    //return JSON.parse(output);
+}
+
+function performRequest(endpoint, method, query) {
+
 }
 
 var day = 1, mon = 1, year = 2000, count = 0;
@@ -177,49 +236,42 @@ function replaceAllDouble(msg, type) {
     return msg;
 }
 
-function crawlGuardian(query) {
-    
-    return performRequest('http://content.guardianapis.com/search', 'GET', query)
-
-    //var GApi = "http://content.guardianapis.com/search?q=";
-    //GApi = GApi + query + "&sort=newest&api-key=neukrcw8u9xm4ks5zejvx3uj";
-    //exports.getJSON(GApi, function (data) {
-    //    $.each(data.response.results, function () {
-    //        var html = 'Date: ' + this.webPublicationDate;
-    //        html += ' abstract: ' + this.sectionName;
-    //        html += ' headlines: ' + this.webTitle;
-    //        html += ' leadparagraph: ' + this.sectionName;
-    //        html += this.webUrl;
-    //       return html				
-    //    });
-    //});
-}
-
-function performRequest(endpoint, method, query) {
-    //var headers = {};
-    //console.log('inside1');
-    if (method == 'GET') {
-        endpoint += '?q=' + query + "&api-key=" + key + "&sort=newest";
-    }
-    //else {
-    //    headers = {
-    //        'Content-Type': 'application/json',
-    //        'Content-Length': dataString.length
-    //    };
-    //}
-    //console.log('inside2');
-    //var options = {
-    //    host: host,
-    //    path: endpoint,
-    //    method: method,
-    //    headers: headers
-    //};
-    //console.log('inside3');
-    
-    
-    requestify.get(endpoint).then(function (response) {
-        return response.getBody().response.results;
-    });
-}
-
 module.exports = router;
+
+
+//fs.writeFile('./timeLine.json', output, function (err) {
+//    if (err)
+//        console.log(err);
+//    else {
+//        console.log('timeLine.json saved!');
+//    }
+//});
+
+////Initial Set up for Solr result
+//if (type == 'solr' ) {
+//    data = result.response.docs;
+//    title = replaceAllDouble(data[0].TITLE, "title");
+//    content = replaceAllDouble(data[0].CONTENT, "content");
+//    output += title + "\x22,\x22type\x22:\x22default\x22,\x22text\x22:\x22<p>";
+//    output += content + "</p>\x22,\x22startDate\x22:\x22" + getDate() + "\x22";
+//}
+
+////Initial Set up if it is guardian, given solr didnt have anything.
+//if (type == 'guardian' && start == 0) {
+//    data = result;
+//    title = replaceAllDouble(data[0].webTitle, "title");
+//    content = replaceAllDouble(data[0].id, "content");
+//    newsDate = data[0].webPublicationDate.substring(0, 10).split("-");
+//    newsDate = newsDate[0] + "," + newsDate[1] + "," + newsDate[2];
+
+//    output += title + "\x22,\x22type\x22:\x22default\x22,\x22text\x22:\x22<p>";
+//    output += content + "</p>\x22,\x22startDate\x22:\x22" + newsDate + "\x22,";
+//    output += "\"tag\":\"" + data[0].sectionName + "\"";
+//    start = 1;
+//}
+
+////In case we only have one record, so we have to close the output here and send.
+//if (Object.keys(data).length == 1) {
+//    output += "}}";
+//    return;
+//}
